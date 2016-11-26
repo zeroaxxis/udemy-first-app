73abfb065e31bb80ffa75132cb9caad8b174925b
Francois Lachance <lachancefrancois@hotmail.com>
2016-11-26 14:51:20 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Inline Template</h1>
+    <fa-other></fa-other>
+    <fa-another>
+        <div>
+          <h1>Hello!</h1>
+          <p>World!</p>
+        </div>
+    </fa-another>
+    <fa-another>
+        <div>Something else...</div>
+    </fa-another>
+  `,
+  styles: [`
+    h1{
+      color:red;
+    }
+  `]
+})
+export class AppComponent {
+  title = 'I changed it!';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { OtherComponent } from './other/other.component';
+import { AnotherComponent } from './other/another.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OtherComponent,
+    AnotherComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
